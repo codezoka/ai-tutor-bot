9647ccc95e3c4d4a74716844b1172bce7a23d58d
codezoka <zoricacekova1@gmail.comgit>
2025-10-17 02:33:35 +0200

    Add working Flask web app

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return "✅ Webhook placeholder is live!"
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8080))  # <— this is the key part
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
